79bdc4da7802487c33293414d809ba5c07215a60
jakub <jakub.sadlowski@gmail.com>
2024-12-08 16:54:58 +0100

    Soft change.

--- Optimalization/Pliki pomocnicze/user_funs.h
+++ Optimalization/Pliki pomocnicze/user_funs.h
@@ -18,3 +18,4 @@ matrix fT3a(matrix, matrix = NAN, matrix = NAN);
 matrix fT3b(matrix, matrix = NAN, matrix = NAN);
 matrix df3(double, matrix, matrix = NAN, matrix = NAN);
 matrix ff3R(matrix, matrix = NAN, matrix = NAN);
+matrix ff4T(matrix, matrix = NAN, matrix = NAN);
